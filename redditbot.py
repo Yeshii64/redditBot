6e2a410a16e6050ff8ab06fa39f71956fc2fe0c4
Yeshii64 <yeshuaafflick5@gmail.com>
2025-04-09 14:56:50 -0400

    gotta incorporate functions!

--- redditbot.py
+++ redditbot.py
@@ -10,10 +10,20 @@ config = ConfigParser()
 path = r"C:\Users\qyut1\OneDrive\Desktop\reddit bot\redditBot\praw.ini"
 config.read(path)
 print(config.read(path))
-print(config.sections())
-print( config["BOT1"]["username"])
+def main():
+    reddit = praw.Reddit(
+        username=config["BOT1"]["username"],
+        user_agent=config["BOT1"]["user_agent"],
+        client_id=config["BOT1"]["client_id"],
+        client_secret=config["BOT1"]["client_secret"],
+        password=config["BOT1"]["password"]
+    )
+    return reddit
+
+print(reddit)
+
+subreddit = reddit.subreddit("learnpython")
+for submission in subreddit.stream.submissions():
+    print(submission.title)
 
 
-#testing 
-#for submission in reddit.submission("test").hot(limit=10):
- #   print(submission.title)
